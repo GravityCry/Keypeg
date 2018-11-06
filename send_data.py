246890f9d00f1fb60f87c8f3754059a68102e043
ShutoAraki <shutoaraki_2020@depauw.edu>
2018-11-06 16:28:20 -0500

    send_data: add pexpect code

--- send_data.py
+++ send_data.py
@@ -1,1 +1,17 @@
-import subprocess
+import subprocess, pexpect
+
+host = 'alucard.csc.depauw.edu'
+dst = '/home/LDAPdir/qnguyen19/'
+user = 'qnguyen19'
+pwd = 'quannguyen135'
+
+def send(filepath):
+    child = pexpect.spawn(f'scp {filepath} {user}@{host}:{dst}')
+    child.expect(f"{user}@{host}s password:'")
+    child.sendline(pwd)
+    child.expect(pexpect.EOF, timeout=None)
+
+    stdout = child.before
+    cmd_output = stdout.split('\r\n')
+    for data in cmd_output:
+        print(data)
