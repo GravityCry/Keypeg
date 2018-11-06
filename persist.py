46e18fc32f6a3db0cd0c2eea6b1a00abc83c3c8a
ShutoAraki <shutoaraki_2020@depauw.edu>
2018-11-06 16:02:23 -0500

    persist: add copy code

--- persist.py
+++ persist.py
@@ -1,14 +1,28 @@
-import os
+import os, shutil
 
-def list_files(startpath):
-    for root, dirs, files in os.walk(startpath):
-        level = root.replace(startpath, '').count(os.sep)
-        indent = ' ' * 4 * level
-        print(f'{indent}{os.path.basename(root)}/')
+FILE_TO_COPY = 'main.exe' # change later to antivirus
 
-        subindent = indent + ' ' * 4
-        for f in files:
-            print(f'{subindent}{f}')
+def find_folder(folder_name, startpath):
+    results = []
+
+    for root, _, _ in os.walk(startpath):
+        base_root = os.path.basename(root)
+        if folder_name == base_root.lower():
+            results.append(root)
+
+    return results
+
+def add_to_startup(startup_path='C:\\Users\\DePauw\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup'):
+    try:
+        shutil.copy(os.path.join(os.getcwd(), FILE_TO_COPY),
+            os.path.join(startup_path, FILE_TO_COPY))
+    except Exception as e:
+        print('An exception occured.')
+        print(e)
 
 if __name__ == '__main__':
-    list_files(os.getcwd())
+    #print(find_folder('python', 'C:\\Users\\DePauw\\Documents\\'))
+    #startup_paths = find_folder('startup', 'C:\\')
+    #print(startup_paths)
+
+    add_to_startup()
